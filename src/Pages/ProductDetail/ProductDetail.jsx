042bac8d778ad4c06d6643fe6f0646cf6c3607c3
Newton Yetsedaw <yetsedawn@gmail.com>
2025-04-07 12:49:41 +0300

    addition of description on single product

--- src/Pages/ProductDetail/ProductDetail.jsx
+++ src/Pages/ProductDetail/ProductDetail.jsx
@@ -28,7 +28,11 @@ function ProductDetail() {
 
   return (
     <LayOut>
-      {isLoading ? <Loader /> : <ProductCard product={product} />}
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <ProductCard product={product} flex={true} renderDesc={true} />
+      )}
     </LayOut>
   );
 }
--- src/components/Product/ProductCard.jsx
+++ src/components/Product/ProductCard.jsx
@@ -3,27 +3,33 @@ import Rating from "@mui/material/Rating";
 import CurrencyFormat from "../CurrencyFormat/CurrencyFormat";
 import classes from "./Product.module.css";
 import { Link } from "react-router";
-function ProductCard({ product }) {
-  const { title, price, category, image, rating } = product;
+function ProductCard({ product, flex, renderDesc }) {
+  const { title, price, category, image, rating, description } = product;
+  console.log(description);
 
   return (
-    <div className={classes.card_container}>
+    <div
+      className={`${classes.card_container} ${
+        flex ? classes.product_flexed : ""
+      }`}
+    >
       <Link to={`/products/${product.id}`}>
         <img src={image} alt={category} />
       </Link>
       <div>
         <h3>{title}</h3>
+        {renderDesc && <div style={{ maxWidth: "750px" }}>{description}</div>}
         <div className={classes.rating}>
           {/* rating */}
           <Rating value={rating?.rate || 0} precision={0.1} />
           {/* rating count */}
           <small>{rating?.count}</small>
         </div>
+        <div className={classes.price}>
+          <CurrencyFormat amount={price} />
+        </div>
+        <button className={classes.button}>add to cart</button>
       </div>
-      <div className={classes.price}>
-        <CurrencyFormat amount={price} />
-      </div>
-      <button className={classes.button}>add to cart</button>
     </div>
   );
 }
