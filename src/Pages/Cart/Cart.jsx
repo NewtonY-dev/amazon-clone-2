92e06c1a6a70967f6e933326f0898fa8eb3edfde
Newton Yetsedaw <yetsedawn@gmail.com>
2025-04-10 13:37:23 +0300

    Cart Page Part Three

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -5,12 +5,28 @@ import { DataContext } from "../../components/DataProvider/DataProvider";
 import ProductCard from "../../components/Product/ProductCard";
 import CurrencyFormat from "../../components/CurrencyFormat/CurrencyFormat";
 import { Link } from "react-router-dom";
+import { Type } from "../../Utility/action.type";
+import { IoIosArrowUp } from "react-icons/io";
+import { IoIosArrowDown } from "react-icons/io";
 function Cart() {
   const [{ basket, user }, dispatch] = useContext(DataContext);
   const total = basket?.reduce((amount, item) => {
     return item.price * item.amount + amount;
   }, 0);
-  console.log(basket);
+
+  const increment = (item) => {
+    dispatch({
+      type: Type.ADD_TO_BASKET,
+      item,
+    });
+  };
+
+  const decrement = (id) => {
+    dispatch({
+      type: Type.REMOVE_FROM_BASKET,
+      id,
+    });
+  };
 
   return (
     <LayOut>
@@ -24,13 +40,29 @@ function Cart() {
           ) : (
             basket?.map((item, i) => {
               return (
-                <ProductCard
-                  key={i}
-                  product={item}
-                  renderDesc={true}
-                  flex={true}
-                  renderAdd={true}
-                />
+                <section key={item.id} className={classes.cart_product}>
+                  <ProductCard
+                    product={item}
+                    renderDesc={true}
+                    flex={true}
+                    renderAdd={true}
+                  />
+                  <div className={classes.btn_container}>
+                    <button
+                      className={classes.btn}
+                      onClick={() => increment(item)}
+                    >
+                      <IoIosArrowUp size={20} />
+                    </button>
+                    <span>{item.amount}</span>
+                    <button
+                      className={classes.btn}
+                      onClick={() => decrement(item.id)}
+                    >
+                      <IoIosArrowDown size={20} />
+                    </button>
+                  </div>
+                </section>
               );
             })
           )}
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,6 +8,9 @@ import { Link } from "react-router-dom";
 import { DataContext } from "../DataProvider/DataProvider";
 function Header() {
   const [{ basket }, dispatch] = useContext(DataContext);
+  const totalItem = basket?.reduce((amount, item) => {
+    return item.amount + amount;
+  }, 0);
 
   return (
     <section className={classes.fixed}>
@@ -55,7 +58,7 @@ function Header() {
             </Link>
             <Link to="/cart" className={classes.cart}>
               <BiCart size={35} />
-              <span>{basket.length}</span>
+              <span>{totalItem}</span>
             </Link>
           </div>
         </div>
