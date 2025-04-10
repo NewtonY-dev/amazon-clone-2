132f43354a52657384236005966548ceaed6757b
Newton Yetsedaw <yetsedawn@gmail.com>
2025-04-10 09:40:27 +0300

    Cart Page Part Two

--- src/Utility/reducer.js
+++ src/Utility/reducer.js
@@ -7,10 +7,26 @@ export const initialState = {
 export const reducer = (state, action) => {
   switch (action.type) {
     case Type.ADD_TO_BASKET:
-      return {
-        ...state,
-        basket: [...state.basket, action.item],
-      };
+      // check if the item already exists in the basket
+      const existingItem = state.basket?.find((item) => {
+        return item.id === action.item.id;
+      });
+      if (!existingItem) {
+        return {
+          ...state,
+          basket: [...state.basket, { ...action.item, amount: 1 }],
+        };
+      } else {
+        const updatedItem = state.basket?.map((item) => {
+          return item.id === action.item.id
+            ? { ...item, amount: item.amount + 1 }
+            : item;
+        });
+        return {
+          ...state,
+          basket: updatedItem,
+        };
+      }
     default:
       return state;
   }
