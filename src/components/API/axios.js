f197a0f948ef05fc23a2be7e6984719a2decf01e
Newton Yetsedaw <yetsedawnewton@gmail.com>
2025-04-25 12:55:14 +0300

    Update baseURL

--- src/components/API/axios.js
+++ src/components/API/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-e5a02/us-central1/api",
+  // local firebase functions/api
+  // baseURL: "http://127.0.0.1:5001/clone-e5a02/us-central1/api",
+
+  // amazon-clone api deployed on render.com 
+  baseURL: "https://amazon-clone-api-wox6.onrender.com",
 });
 
 export { axiosInstance };
